df0019eda81eeaadf885a8401e3a0c0a713e08b3
TLSenz <sven@zemp.email>
2024-11-04 12:16:36 +0100

    3.Aufgabe max Zahl

---
+++ C-Sharp-gibb/Uebung_04_7_Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_04_7_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int biggest = 0;
+            int placeholder = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine((i + 1) + ". Zahl");
+                placeholder = Convert.ToInt32(Console.ReadLine());
+                if (placeholder > biggest)
+                {
+                    biggest = placeholder;
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            Console.WriteLine("Die grösste Zahl: "+biggest);
+            Console.ReadLine();
+                    
+
+        }
+    }
+}
---
+++ C-Sharp-gibb/Uebung_04_7_Arrays/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Uebung_04_7_Arrays")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Uebung_04_7_Arrays")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("5fe2cb3e-343a-4af6-b00d-935dd61a9363")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
