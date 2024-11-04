5f159163a3d96b5be67b37e35e9963bd3d0ea911
TLSenz <sven@zemp.email>
2024-11-04 10:24:37 +0100

    Project 1

---
+++ C-Sharp-gibb/Helloworld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie Heiss ist es in Celsius: ");
+            int TemperaturCelsius = Convert.ToInt32(Console.ReadLine());
+            int TemperaturFahrenheit = ((TemperaturCelsius * 9) / 5) + 32;
+            Console.WriteLine(TemperaturFahrenheit);
+
+        }
+    }
+}
